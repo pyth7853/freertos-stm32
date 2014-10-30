6cac72b8a0b273dcae8319dca3f09a426089aa22
MING-YI <pyth7853@gmail.com>
2014-10-30 22:39:56 +0800

    Gyroscope: Init Gyroscope

--- CORTEX_M4F_STM32F4/main.c
+++ CORTEX_M4F_STM32F4/main.c
@@ -255,6 +255,24 @@ static void UsartTask(void *pvParameters)
         USART_SendData(USART1, t);		
     }
 }
+static void Gyroscope_Init(void)
+{
+	L3GD20_InitTypeDef L3GD20_InitStructure;
+	L3GD20_InitStructure.Power_Mode = L3GD20_MODE_ACTIVE;
+	L3GD20_InitStructure.Output_DataRate = L3GD20_OUTPUT_DATARATE_1;
+	L3GD20_InitStructure.Axes_Enable = L3GD20_AXES_ENABLE;
+	L3GD20_InitStructure.Band_Width = L3GD20_BANDWIDTH_4;
+	L3GD20_InitStructure.BlockData_Update = L3GD20_BlockDataUpdate_Continous;
+	L3GD20_InitStructure.Endianness = L3GD20_BLE_LSB;
+	L3GD20_InitStructure.Full_Scale = L3GD20_FULLSCALE_250;
+	L3GD20_Init(&L3GD20_InitStructure);
+
+	L3GD20_FilterConfigTypeDef L3GD20_FilterStructure;
+	L3GD20_FilterStructure.HighPassFilter_Mode_Selection = L3GD20_HPM_NORMAL_MODE_RES;
+	L3GD20_FilterStructure.HighPassFilter_CutOff_Frequency = L3GD20_HPFCF_0;
+	L3GD20_FilterConfig(&L3GD20_FilterStructure);
+	L3GD20_FilterCmd(L3GD20_HIGHPASSFILTER_ENABLE);
+}
 static void GyroscopeTask(void *pvParameters)
 {
 	Gyroscope_Init();
